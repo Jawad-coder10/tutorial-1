62ff5c21261676bf8ee76249880a653f26ff0182
Jawad-coder10 <djawadisaindou@gmail.com>
2025-03-08 15:11:20 +0000

    feat: Use state counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Counter() {
+  const [counter, setCounter]= useState(0)
+
   return (
+    <>
     <div className='flex gap-5 items items-center'>
-      <button className='px-10 py-2 bg-slate-800 text-white'> - </button>
-      <p> 0 </p>
-      <button className='px-10 py-2 bg-slate-800 text-white'> + </button>
+      <button 
+        onClick={()=>setCounter(counter-5)}
+      className='px-10 py-2 bg-slate-800
+       text-white'> - </button>
+      <p> {counter} </p>
+      <button
+         onClick={()=>setCounter(counter+5)}
+      className='px-10 py-2 bg-slate-800 text-white'> + </button>
+      <button
+         onClick={()=>setCounter(0)}
+      className='px-10 py-2 bg-red-400 text-white'> reset </button>
     </div>
+    
+    </>
   )
 }
