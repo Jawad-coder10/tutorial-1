1f4078993afe013e2aad84aa7356cc5802baa4be
Jawad-coder10 <djawadisaindou@gmail.com>
2025-03-08 15:22:21 +0000

    feat: useEffect counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,7 +1,15 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 export default function Counter() {
   const [counter, setCounter]= useState(0)
+
+  useEffect(() => {
+
+    setTimeout(() => {
+      setCounter(counter+1)
+    }, (2000));
+
+  });
 
   return (
     <>
