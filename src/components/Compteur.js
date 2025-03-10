12f9b0c80cd8fe1db56b3279a247240f4e90a83b
ibrahimabdouammarat <ibrahimabdouammarat2gmail.com>
2025-03-10 14:35:07 +0100

    feat: counter

--- src/components/Compteur.js
+++ src/components/Compteur.js
@@ -13,13 +13,13 @@ export default function Compteur() {
     <div className='h-full mt-16 flex flex-col items-center justify-center'>
     <div className='flex gap-5 items items-center'>
       <button
-      onClick={() => { setCounter(-1) }} 
+      onClick={() => { setCounter(counter-1) }} 
       className='px-10 py-2 bg-slate-800
        text-white'> - </button>
       <p> {counter} </p>
       <button
       className='px-10 py-2 bg-slate-800 text-white'
-      onClick={() => { setCounter(1) }} 
+      onClick={() => { setCounter(counter+1) }} 
       >
          + </button>
       <button
